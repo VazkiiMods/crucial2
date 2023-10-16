a0012a91b1e9fbe2a09af9912e320f0d02933269
Vasco Lavos <vazkii@hotmail.com>
2023-10-16 18:17:05 +0100

    Fix #350

--- ship/ship.py
+++ ship/ship.py
@@ -24,8 +24,7 @@ server_mods_blacklist = [
 	460819, # LazyDFU
 	441114, # Neko's Enchanted Books
 	532127, # Legendary Tooltips
-	574856, # Rubidium
-	558905  # Magnesium Extras
+	825358  # Embeddium Extras
 ]
 server_blacklisted_files = [
 	'openloader/resources',
